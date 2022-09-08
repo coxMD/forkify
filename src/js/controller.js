9209a29633cda94b357fb862c441038440c78fe3
coxMD <coxmusyoki@gmail.com>
2022-09-08 11:48:33 +0300

    Modified architecture to MVC and implemented the recipe API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable'; ///For polyphilying everything else
+import 'regenerator-runtime/runtime'; //For polyphilying async await
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +17,28 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //Loading recipe
+    await model.loadRecipe(id);
+
+
+    //2 Rendering recipe
+    recipeView.render(model.state.recipe);
+
+
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+
+// window.addEventListener('hashchange', controlRecipes());
+// window.addEventListener('load', controlRecipes())
